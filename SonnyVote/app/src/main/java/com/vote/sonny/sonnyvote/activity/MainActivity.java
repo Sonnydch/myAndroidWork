f6661a7dc1b6894f55994908ca75890d4f80b3e4
Sonny <sunnydch999@gmail.com>
2016-03-26 11:11:05 +0800

    Add sonny Vote

---
+++ SonnyVote/app/src/main/java/com/vote/sonny/sonnyvote/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vote.sonny.sonnyvote.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vote.sonny.sonnyvote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
